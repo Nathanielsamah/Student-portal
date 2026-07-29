bebebf4d21291dedbcadc2698a31d06099d293e2
NATHANIEL SAMAH <115565609+Nathanielsamah@users.noreply.github.com>
2026-07-29 09:26:05 -0700

    Update project.py

--- project.py
+++ project.py
@@ -19,11 +19,11 @@ st.markdown("---")
 if 'student_db' not in st.session_state:
     # This data is fully completed with real values to guarantee zero errors
     initial_data = {
-        "Roll Num": [222787, 222788, 222789,],
-        "Name": ["Alphonso Kamara", "Blessing Flomo", "Emmanuel Kollie"],
-        "Department": ["IT", "Computer Science", "Cyber Security"],
-        "Marks (%)": [85, 92, 78,],
-        "Grade": ["A", "A+", "B"]
+        "Roll Num": [222787, 222788, 222789, 222902],
+        "Name": ["Alphonso Kamara", "Blessing Flomo", "Emmanuel Kollie", "Smith Samah"],
+        "Department": ["IT", "Computer Science", "Cyber Security", "Medical Lab Science"],
+        "Marks (%)": [85, 92, 78, 88],
+        "Grade": ["A", "A+", "B", "C"]
     }
     st.session_state.student_db = pd.DataFrame(initial_data)
 
