1861283811bb550c4f6be49c0ed0dad5bc369df6
NATHANIEL SAMAH <115565609+Nathanielsamah@users.noreply.github.com>
2026-08-03 09:08:35 -0700

    Update project.py

--- project.py
+++ project.py
@@ -96,7 +96,7 @@ with col_form:
     with st.form("student_entry_form", clear_on_submit=True):
         roll_num = st.number_input("Roll Number", min_value=1, step=1, value=104)
         student_name = st.text_input("Student Full Name", placeholder="Enter name here")
-        department = st.selectbox("Department", ["IT", "Cyber Security", "Cloud Computing", "Computer Science"])
+        department = st.selectbox("Department", ["IT", "Cyber Security", "Cloud Computing", "Computer Science", "Economics"])
         marks = st.slider("Total Marks (%)", min_value=0, max_value=100, value=75)
         grade = st.selectbox("Final Grade", ["A+", "A", "B+", "B", "C", "Fail"])
         
