ee901eac0f93bef0d16f96bd7e36c41841a28e19
NATHANIEL SAMAH <115565609+Nathanielsamah@users.noreply.github.com>
2026-08-10 00:27:26 -0700

    Update project.py

--- project.py
+++ project.py
@@ -5,9 +5,6 @@ import json
 from io import BytesIO  # Handles file memory for the Excel download
 from streamlit_local_storage import LocalStorage  # Saves data directly to browser hard drive
 
-# --- CONFIGURATION: SET YOUR MASTER CREDENTIALS HERE ---
-
-
 # 1. Page Configuration and Theme Styling
 st.set_page_config(
     page_title="Professor's Student Records Portal",
